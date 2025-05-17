a4455d8e2902dc0db698d572072d3cee40a72820
luvbigpig <128917254+luvbigpig@users.noreply.github.com>
2025-05-17 12:20:56 +0700

    Add files via upload

---
+++ epg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+. /etc/profile
+
+
+wget -O $HOME/xmltv.xmltv http://lichphatsong.xyz/schedule/epg.xml
+/data/data/com.termux/files/usr/bin/socat open:$HOME/xmltv.xmltv UNIX-CONNECT:/data/data/com.termux/files/home/.hts/tvheadend/epggrab/xmltv.sock
+
